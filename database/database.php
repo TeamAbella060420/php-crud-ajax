7cbffdb7fb6e8daff57acf5aeea2290bb7b87570
TeamAbella060420 <teamabella@outlook.com>
2023-06-10 18:50:24 +0800

    Update database.php

--- database/database.php
+++ database/database.php
@@ -1,21 +1,27 @@
 <?php
-$servername = "localhost"; // Replace with your server name
-$username = "your_username"; // Replace with your username
-$password = "your_password"; // Replace with your password
-$database = "your_database"; // Replace with your database name
 
-try {
-    // Create a new PDO instance
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+class Database
+{
+    private $servername = "localhost"; // Replace with your server name
+    private $username = "your_username"; // Replace with your username
+    private $password = "your_password"; // Replace with your password
+    private $database = "your_database"; // Replace with your database name
+    public $conn = '';
 
-    // Set PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    public function __construct()
+    {
+        try {
+            // Create a new PDO instance
+            $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->database", $this->username, $this->password);
 
-    echo "Connected successfully";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+            // Set PDO error mode to exception
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            echo "Connected successfully";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
 }
 
-// Close the connection
-$conn = null;
 ?>
