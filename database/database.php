5baa0a1e752ab78ba1a3e852256f5d25a99689b9
TeamAbella060420 <teamabella@outlook.com>
2023-06-10 12:50:32 +0800

    Update database.php

--- database/database.php
+++ database/database.php
@@ -1,3 +1,21 @@
 <?php
-    echo('Hello World!')
+$servername = "localhost"; // Replace with your server name
+$username = "your_username"; // Replace with your username
+$password = "your_password"; // Replace with your password
+$database = "your_database"; // Replace with your database name
+
+try {
+    // Create a new PDO instance
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+
+    // Set PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+// Close the connection
+$conn = null;
 ?>
