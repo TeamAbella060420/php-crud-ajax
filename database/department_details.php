920b26cdf9f1296d5680b4688b4e5c96942d938d
TeamAbella060420 <teamabella@outlook.com>
2023-06-12 15:40:23 +0800

    Update department_details.php

--- database/department_details.php
+++ database/department_details.php
@@ -4,13 +4,14 @@ require_once('database.php');
 
 $dbo = new Database;
 
-$cmd = "insert into department_details (title, code)";
+$cmd = "insert into department_details (title, code) values('Department of Electronics','ECE')";
+$stmnt = $dbo->$conn->prepare($cmd);
+$stmnt->execute();
+
+
 $cmd = "select * from department_details";
-
 $statement = $dbo->$conn->prepare($cmd);
-
 $statement->execute();
-
 $returnValue = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 print_r($returnValue);
