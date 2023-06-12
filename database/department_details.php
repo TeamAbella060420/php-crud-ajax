32ffc69587cf6f5c510be35b313a1e64a765067f
TeamAbella060420 <teamabella@outlook.com>
2023-06-12 15:36:37 +0800

    Update department_details.php

--- database/department_details.php
+++ database/department_details.php
@@ -1,16 +1,16 @@
-<?php
+<?php 
 
-require_once "database.php";
-// create an object to make a connection to the database
-$dbo = new Database();
-//how to execute sql commands?
-//a. write the string version of the command
+require_once('database.php');
+
+$dbo = new Database;
+
+$cmd = "insert into department_details";
 $cmd = "select * from department_details";
-//b. create a prepared statement
-$statement = $dbo->conn->prepare($cmd);
-//c. execute the prepared statement
+
+$statement = $dbo->$conn->prepare($cmd);
+
 $statement->execute();
-//d. view the result
+
 $returnValue = $statement->fetchAll(PDO::FETCH_ASSOC);
-//e. display the result 
+
 print_r($returnValue);
