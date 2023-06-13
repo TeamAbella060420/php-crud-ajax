aa97ad2063b7fd328da109daad251d7e99719627
TeamAbella060420 <teamabella@outlook.com>
2023-06-13 13:34:34 +0800

    Update getprogrammedetails.js

--- js/getprogrammedetails.js
+++ js/getprogrammedetails.js
@@ -1,5 +1,17 @@
+function getprogrammedetails() {
+    $.ajax({
+    url: "",
+    type: "",
+    dataType: "json",
+
+    
+    });
+}
+
+
 $(document).ready(
-    function name(params) {
-        alert('Jquery loaded!')
+    function name() {
+        alert('Jquery loaded!');
+        getprogrammedetails();
     }
 );
