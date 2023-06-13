8bf0596786ec6a7e4f8ae1ca387122df1a2268bc
TeamAbella060420 <teamabella@outlook.com>
2023-06-13 13:00:03 +0800

    Update getprogrammedetails.php

--- UI/getprogrammedetails.php
+++ UI/getprogrammedetails.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/getprogrammedetails.css">
 </head>
 <body>
-    
+    <main>
+        <div>
+            <h1>PROGRAMME DETAILS</h1>
+        </div>
+    </main>
 </body>
 </html>
