9f786bc44001b8b4d9379a1c2299bd185f8becfd
TeamAbella060420 <teamabella@outlook.com>
2023-06-15 17:21:53 +0800

    Update getprogrammedetailsAJAX.php

--- ajax/getprogrammedetailsAJAX.php
+++ ajax/getprogrammedetailsAJAX.php
@@ -3,12 +3,6 @@
 
 //a: "21", b: "flower", action: "getprogrammedetails"
 
-$root = $_SERVER["DOCUMENT_ROOT"];
-
-include_once $root."../database/database.php";
-include_once $root."../database/programme_details.php";
-include_once $root."../database/department_details.php";
-
 $p = $_POST["a"];
 $q = $_POST["b"];
 $action = $_POST["action1"];
