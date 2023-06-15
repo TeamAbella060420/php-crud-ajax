d241cc9c19046d81abdb8fc3b1616207b427e667
TeamAbella060420 <teamabella@outlook.com>
2023-06-15 18:58:07 +0800

    Update getprogrammedetails.js

--- js/getprogrammedetails.js
+++ js/getprogrammedetails.js
@@ -1,26 +1,23 @@
 function getprogrammedetails() {
     $.ajax({
-    url: "../ajax/getprogrammedetailsAJAX.php",
-    type: "POST",
-    dataType: "JSON",
-    data: {a: "21", b: "flower", action1: "getprogrammedetails"},
-    beforeSend: function() {
-        alert("about to make an ajax call");
-    },
-    success: function (res) {
-           let x =  JSON.stringify(res);
-           alert(x);
-    },
-    error: function () {
-        alert('Error')
-    }
+        url: "../ajax/getprogrammedetailsAJAX.php",
+        type: "POST",
+        dataType: "JSON",
+        data: {a: "21", b: "flower", action: "getprogrammedetails"},
+        beforeSend: function() {
+            alert("About to make an AJAX call");
+        },
+        success: function (res) {
+            let x = JSON.stringify(res);
+            alert(x);
+        },
+        error: function () {
+            alert('Error occurred');
+        }
     });
 }
 
-
-$(document).ready(
-    function name() {
-        alert('Jquery loaded!');
-        getprogrammedetails();
-    }
-);
+$(document).ready(function() {
+    alert('jQuery loaded!');
+    getprogrammedetails();
+});
