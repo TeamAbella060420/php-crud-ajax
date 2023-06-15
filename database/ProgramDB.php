e7678285355e0908f5aa32547fe0aff14434821a
TeamAbella060420 <teamabella@outlook.com>
2023-06-15 18:58:05 +0800

    Update ProgramDB.php

--- database/ProgramDB.php
+++ database/ProgramDB.php
@@ -4,7 +4,17 @@ require_once "database.php";
 
 class ProgramDB 
 {
-
+    public function getAllPrograms($dbo) 
+    {
+        $cmd = "select 
+        pd.id as pid,
+        pd.code as pcode,
+        pd.title as ptitle,
+        pd.no_of_sem as nos,
+        pd.graduation_level as gl,
+        pd.technical_level as tl,
+        department_details as dd where pd.department_id=dd.id";
+    }
 }
 
 ?>
