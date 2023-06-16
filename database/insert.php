3e803867c53ffe287b6e3204f4d2efbcd778b7fe
TeamAbella060420 <teamabella@outlook.com>
2023-06-16 16:00:42 +0800

    Delete insert.php

--- database/insert.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('database.php');
-
-$dbo = new Database;
-
-$cmd = "insert into department_details (title, code) values('Department of Electrical','EE')";
-$statement = $dbo->conn->prepare($cmd);
-$statement->execute();
