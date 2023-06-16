199ffbaea69f25185c74457a073dacc8f65006c2
TeamAbella060420 <teamabella@outlook.com>
2023-06-16 13:22:33 +0800

    Update p4test.php

--- database/p4test.php
+++ database/p4test.php
@@ -6,13 +6,16 @@ require_once "ProgramDB.php";
 $dbo = new Database();
 $pdo = new ProgramDB();
 
-$rv = $pdo->getAllPrograms($dbo);
+// $rv = $pdo->getAllPrograms($dbo);
+// print_r($rv);
+// echo("<br>");
+// $rv = $pdo->createNewProgram($dbo, "CSE", "BTECH in Electronics", 8, "UG", "BTECH", 3); 
+// echo($rv);
+// echo("<br>");
+// $rv = $pdo->getAllPrograms($dbo);
+// print_r($rv);
+$rv = $pdo->getProgramDetailsById($dbo, 120);
 print_r($rv);
 echo("<br>");
-$rv = $pdo->createNewProgram($dbo, "CSE", "BTECH in Electronics", 8, "UG", "BTECH", 3); 
-echo($rv);
-echo("<br>");
-$rv = $pdo->getAllPrograms($dbo);
-print_r($rv);
 
 ?>
