e452b0f823c7c22d50d10fb4e9f46a3718ec5434
TeamAbella060420 <teamabella@outlook.com>
2023-06-17 18:02:52 +0800

    Update database.php

--- database/database.php
+++ database/database.php
@@ -16,7 +16,7 @@ class Database
 
             // Set PDO error mode to exception
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
+            // echo "Connected successfully";
         } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
         }
