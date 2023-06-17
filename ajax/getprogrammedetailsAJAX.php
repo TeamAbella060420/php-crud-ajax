2192889da85a551371965addf4f4630767f43282
TeamAbella060420 <teamabella@outlook.com>
2023-06-17 15:29:12 +0800

    Update getprogrammedetailsAJAX.php

--- ajax/getprogrammedetailsAJAX.php
+++ ajax/getprogrammedetailsAJAX.php
@@ -1,14 +1,22 @@
 <?php
 
+// $root = $_SERVER["DOCUMENT_ROOT"];
+include_once "/php-crud-ajax/database/database.php";
+include_once "/php-crud-ajax/database/ProgramDB.php";
 
 $p = $_POST["a"];
 $q = $_POST["b"];
-$action = $_POST["action"];
+$action = $_POST["action1"];
 
-if($action == "getprogrammedetails") {
-  $result = [$p, $q, "from server"];
+if($action == "getprogrammedetails")
+ {
+  $dbo = new Database();
+  $pdo = new ProgramDB();
+
+  $result = ["ubat"];
   $rv = json_encode($result);
-  echo $rv;
+
+  echo($rv);
   exit();
 }
 
