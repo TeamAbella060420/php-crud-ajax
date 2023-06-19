925adb0f77bd0771ee54140b6b76188010eafae6
TeamAbella060420 <teamabella@outlook.com>
2023-06-19 20:47:57 +0800

    Update getprogrammedetails.php

--- UI/getprogrammedetails.php
+++ UI/getprogrammedetails.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../css/getprogrammedetails.css">
+    <link rel="stylesheet" href="../global/bootstrap.min.css">
 </head>
 <body>
     <main>
-        <div>
+        <div class="text-info text-center"> 
             <h1>PROGRAMME DETAILS</h1>
         </div>
         <div id="contentdiv" class="test1">
@@ -17,6 +18,7 @@
         <div>
             <script src="../global/jqeury.js"></script>
             <script src="../js/getprogrammedetails.js"></script>
+            <script src="../global/bootstrap.bundle.min.js"></script>
         </div>
     </main>
 </body>
