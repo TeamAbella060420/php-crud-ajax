9ccf3efa2255c6b545464cab7dcffc50c8814ebc
TeamAbella060420 <teamabella@outlook.com>
2023-06-19 20:47:55 +0800

    Update getprogrammedetails.js

--- js/getprogrammedetails.js
+++ js/getprogrammedetails.js
@@ -1,42 +1,61 @@
 function getHTML(result) {
-    let x = `
-        <table>
-            <thead>
-                <th>SLNO</th>
-                <th>CODE</th>
-                <th>TITLE</th>
-                <th>Department</th>
-            </thead>
-            <tbody>
-            
-            </tbody>
-        </table>
-    `;
+    let x = "";
+    x += `<div><button class="btn btn-primary">ADD NEW</button></div>`;
+    x += `<table class="table table-striped">`;
+    x += `<thead>
+        <th>SLNO</th>
+        <th>CODE</th>
+        <th>TITLE</th>
+        <th>DPT</th>
+        <th>#SEM</th>
+        <th>GRDTN_LVL</th>
+        <th>TECH_LVL</th>
+        <th></th>
+    </thead>
+    <tbody>`;
+    x += result.map((item, index) => {
+        return `<tr>
+            <td>${index + 1}</td>
+            <td>${item['pcode']}</td>
+            <td>${item['ptitle']}</td>
+            <td>${item['dcode']}</td>
+            <td>${item['nos']}</td>
+            <td>${item['gl']}</td>
+            <td>${item['tl']}</td>
+            <td><button type="button" class=" btn btn-primary">EDIT</button> <button type="button" class="btn btn-danger">DELETE</button></td>
+        </tr>`;
+    }).join("");
+    x += `</tbody></table>`;
+    
+    return x;
 }
+
 
 function getprogrammedetails() {
-        $.ajax({
-            url: "../ajax/getprogrammedetailsAJAX.php",
-            method: "POST",
-            datatype: "json",
-            data: {a: "abab", b: "mommy", action1: "getprogrammedetails"},
-            // beforeSend: function() {
-            //     alert("About to make an AJAX call");
-            // },
-            success: function (res) {
-                let x = JSON.stringify(res);
-                // alert(x);
-                let html = getHTML(result);
-            },
-            error: function (error) {
-                console.log(error);
-                alert('Error occurred');
+    $.ajax({
+        url: "../ajax/getprogrammedetailsAJAX.php",
+        datatype: "JSON",
+        method: "POST",
+        data: { a: "abab", b: "mommy", action1: "getprogrammedetails" },
+        // beforeSend: function() {
+        //     alert("About to make an AJAX call");
+        // },
+        success: function (result) {
+            let y = JSON.parse(result)
+            // alert(y);
+            // alert(html)
+            $("#contentdiv").html(getHTML(y));
+            // console.log(getHTML(result));
+        },
+        error: function (error) {
+            console.log(error);
+            alert('Error occurred');
 
-            }
-        });
+        }
+    });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     // alert('jQuery loaded!');
     getprogrammedetails();
 });
