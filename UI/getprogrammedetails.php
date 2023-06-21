477bdb518723f65e1b74569156dc11e51317b263
TeamAbella060420 <teamabella@outlook.com>
2023-06-21 17:36:11 +0800

    Update getprogrammedetails.php

--- UI/getprogrammedetails.php
+++ UI/getprogrammedetails.php
@@ -6,17 +6,33 @@
     <title>Document</title>
     <link rel="stylesheet" href="../css/getprogrammedetails.css">
     <link rel="stylesheet" href="../global/bootstrap.min.css">
-    <link href="https://cdn.remixicon.com/releases/v1.2.2/remixicon.css" rel="stylesheet">
 </head>
 <body>
     <main>
         <div class="header"> 
-            PROGRAMME DETAILS
+            PROGRAMME DETAILS 
         </div>
         <div id="contentdiv" class="test1">
-
+            
+        </div>
+        <div class="modal" id="modalprogram">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        PROGRAMME DETAILS
+                        <button class="btn btn-danger">X</button>
+                        
+                    </div>
+                    <div class="modal-body"></div>
+                    <div class="modal-footer">
+                        <button class="btn btn-danger">Cancel</button>
+                        <button class="btn btn-success">Save</button>
+                    </div>
+                </div>
+            </div>
         </div>
         <div>
+            <link rel="stylesheet" href="../global/all.min.css">
             <script src="../global/jqeury.js"></script>
             <script src="../js/getprogrammedetails.js"></script>
             <script src="../global/bootstrap.bundle.min.js"></script>
